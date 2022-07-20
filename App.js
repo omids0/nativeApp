2ec6538cfa143a8c41f7bada68a07ec4e22e3f0c
omids0 <soheilniao@gmail.com>
2022-07-20 23:25:30 +0430

    scrollview added

--- App.js
+++ App.js
@@ -1,11 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState("");
+
+  function inputHandler(event) {
+    setTitle(event.target.value);
+  }
+
+  function submitHandler() {
+    setNotes([
+      ...notes,
+      { note: title, id: Math.random().toString(16).slice(2) },
+    ]);
+    // setTitle("");
+  }
+
+  function removeHandler(id) {
+    const notesFilter = notes.filter((note) => note.id !== id);
+    console.log("notesFilter");
+  }
+
+  console.log(notes);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        placeholder="Take your note here..."
+        style={styles.inputNote}
+        onChange={inputHandler}
+        value={title}
+      />
+      <Button title="Add Note" color="orange" onPress={submitHandler} />
+      <View style={styles.listNotes}>
+        <ScrollView>
+          {notes?.map((note) => (
+            <View
+              key={Math.random().toString(16).slice(2)}
+              style={styles.eachNote}
+            >
+              <Text style={styles.textNote} onPress={removeHandler(note.id)}>
+                {note.note}
+              </Text>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -13,8 +62,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    padding: "1rem",
+  },
+  inputNote: {
+    padding: "8px",
+    backgroundColor: "#cfcfcf",
+    marginBottom: "1rem",
+  },
+  listNotes: {
+    marginVertical: "1rem",
+  },
+  eachNote: {
+    backgroundColor: "blue",
+    borderRadius: "8px",
+    padding: "1rem",
+    marginVertical: ".5rem",
+  },
+  textNote: {
+    color: "#fff",
   },
 });
